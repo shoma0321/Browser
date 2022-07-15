caf6d61b98a0ae15c67ad42f0ce0a66b8c8e2f15
shoma <shoma0321prgm@gmail.com>
2022-07-15 17:13:29 +0900

    IBOutlet,IBActionの紐付け

--- Browzer/ViewController.swift
+++ Browzer/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let url = URL(string: "http://www.google.com") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        webView.navigationDelegate = self
     }
-
+    
+    @IBAction func reloadTapped(_ sender: UIButton) {
+        webView.reload()
+    }
+    
+    @IBAction func backTapped(_ sender: UIButton) {
+        webView.goBack()
+    }
+    
+    
+    @IBAction func forwardTapped(_ sender: UIButton) {
+        webView.goForward()
+    }
+    
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("Start!")
+    }
+    
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("Finish!")
+    }
+    
 
 }
 
